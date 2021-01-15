beacc81fad96c3834e893ab8c4d03d67aca38e2f
Satoru Tanaka <ittimfn@gmail.com>
2021-01-15 19:07:01 +0900

    client修正

--- 05_HTTP_Server_and_Client/client.js
+++ 05_HTTP_Server_and_Client/client.js
@@ -2,9 +2,28 @@
 
 const http = require('http');
 
-http.request('http://localhost:3000/api/todos', res => {
+const url = 'http://localhost:3000/api/todos';
+
+// GET
+http.request(url, res => {
     let responseData = '';
+    console.log('GET', url);
     console.log('statusCode', res.statusCode);
     res.on('data', chunk => responseData += chunk);
     res.on('end', () => console.log('responseData', JSON.parse(responseData)));
+}).end();
+
+// POST
+http.request(
+    url,
+    {method: 'POST'},
+    res => {
+        console.log('POST', url);
+        console.log('statusCode', res.statusCode);
+    }).end();
+
+const notFoundUrl = 'http://localhost:3000/api/hogehoge';
+http.request(notFoundUrl, res => {
+    console.log('GET', notFoundUrl);
+    console.log('statusCode', res.statusCode);
 }).end();
