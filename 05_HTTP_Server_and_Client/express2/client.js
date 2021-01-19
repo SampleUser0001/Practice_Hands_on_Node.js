8f7fce68a1e5d11a5b67da7803417466467396da
Satoru Tanaka <ittimfn@gmail.com>
2021-01-19 18:51:47 +0900

    express2用client作成

---
+++ 05_HTTP_Server_and_Client/express2/client.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const http = require('http');
+
+const url = 'http://localhost:3000/api/todos/1';
+
+// GET
+http.request(url, res => {
+    let responseData = '';
+    console.log('GET', url);
+    console.log('statusCode', res.statusCode);
+    res.on('data', chunk => responseData += chunk);
+    res.on('end', () => console.log('responseData', JSON.parse(responseData)));
+}).end();
+
+// POST
+http.request(
+    url,
+    {method: 'POST'},
+    res => {
+        console.log('POST', url);
+        console.log('statusCode', res.statusCode);
+    }).end();
+
+const notFoundUrl = 'http://localhost:3000/api/hogehoge';
+http.request(notFoundUrl, res => {
+    console.log('GET', notFoundUrl);
+    console.log('statusCode', res.statusCode);
+}).end();
