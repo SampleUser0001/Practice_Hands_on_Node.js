79cff81ba0c7786120c286371607e232001b97e3
Satoru Tanaka <ittimfn@gmail.com>
2021-01-26 19:52:07 +0900

    SSEが動くが、エラーが出る。

--- 06_Realtime_Web_application.md
+++ 06_Realtime_Web_application.md
@@ -4,6 +4,9 @@
   - [ポーリング](#ポーリング)
   - [ロングポーリング](#ロングポーリング)
   - [SSE(Server Sent Events)](#sseserver-sent-events)
+    - [実装](#実装)
+      - [ブラウザを開きながらREPLでTODOを追加](#ブラウザを開きながらreplでtodoを追加)
+      - [原因調査](#原因調査)
 
 ## ポーリング
 
@@ -19,3 +22,56 @@
 一度確立したHTTP接続を維持したまま、データ更新が発生するたびにサーバからデータを送る。  
 この本ではこれを実装する。
 
+### 実装
+
+```
+cd 06_Realtime_Web_application/todo-sse
+npm install isomorphic-fetch
+npm install
+npm start
+```
+
+[http://localhost:3000](http://localhost:3000)
+
+ハンズオンNode.jsから引用。
+```
+Google ChromeのDevToolsを開いた状態でhttp://localhost:3000にアクセスします。DevToolsの［Network］タブ内に/api/todos/eventsへのリクエストが表示され、これを選択してさらにその中の［EventStream］タブを開くと、SSEの通信の内容が図6-4のように表示されます。
+```
+…と書いてあるが、eventsへのリクエスト出ないぞ？
+
+#### ブラウザを開きながらREPLでTODOを追加
+
+```
+cd 06_Realtime_Web_application/todo-sse
+npm install isomorphic-fetch
+```
+
+```
+node --experimental-repl-await
+```
+
+```
+require('isomorphic-fetch')
+
+.editor
+
+await fetch('http://localhost:3000/api/todos', {
+  method: 'POST',
+  headers: {
+  'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ title: 'ペン入れ' })
+})
+```
+Ctrl + D
+
+サーバに「ペン入れ」タスクが追加され、ブラウザが更新自動で更新される。  
+**正しく動いているっぽいのに、eventsがブラウザの開発ツールで出ないぞ？**
+
+#### 原因調査
+
+エラーが発生している。
+
+```
+GET http://localhost:3000/api/todos/events net::ERR_INCOMPLETE_CHUNKED_ENCODING 200 (OK)
+```
---
+++ 06_Realtime_Web_application/todo-sse/components/Todos.js
@@ -0,0 +1,63 @@
+'use strict'
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import 'isomorphic-fetch';
+
+// 各ページに関する情報の定義
+const pages = {
+    index: { title: 'すべてのToDo', fetchQuery: '' },
+    active: { title: '未完了のToDo', fetchQuery: '?completed=false' },
+    completed: { title: '完了したToDo', fetchQuery: '?completed=true' }
+};
+
+
+const pageLinks = Object.keys(pages).map((page, index) => 
+    <Link href={`/${page === 'index' ? '' : page}`} key = {index}>
+        <a style={{marginRight: 10}}>{pages[page].title}</a>
+    </Link>
+);
+
+// Reactコンポーネントの実装
+export default function Todos(props){
+    const {title, fetchQuery} = pages[props.page];
+    const[todos, setTodos] = useState([]);
+    useEffect(() => {
+        // EventSourceを使用した実装
+        const eventSource = new EventSource('/api/todos/events');
+        // SSE受信時の処理
+        eventSource.addEventListener('message', e => {
+            const todos = JSON.parse(e.data);
+            setTodos(
+                typeof completed === 'undefined'
+                ? todos
+                : todos.filter(todo => todo.completed === completed )
+            );
+        });
+
+        // エラーハンドリング
+        eventSource.addEventListener('error', e => console.log('SSEエラー', e));
+
+        return () => eventSource.close();
+    }, [props.page])
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <h1>{title}</h1>
+            {/* ToDo一覧の表示 */}
+            <ul>
+                {todos.map(({id, title, completed}) => 
+                    <li key={id}>
+                        <span style={completed ? {textDecoration: 'line-through'} : {}}>
+                            {title}
+                        </span>
+                    </li>
+                )}
+            </ul>
+            <div>{pageLinks}</div>
+        </>
+    )
+};
