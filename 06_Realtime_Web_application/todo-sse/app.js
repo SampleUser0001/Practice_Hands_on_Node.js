8f6ba2df7d3de6442a0d7f426c4b1c9f278678a4
Satoru Tanaka <ittimfn@gmail.com>
2021-02-01 18:47:40 +0900

    差分吸収

--- 06_Realtime_Web_application/todo-sse/app.js
+++ 06_Realtime_Web_application/todo-sse/app.js
@@ -1,104 +1,140 @@
 'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser')
-
+const express = require('express')
 
 let todos = [
-    {id:1, title:'ネーム', completed:false},
-    {id:2, title:'下書き', completed:true}
-];
-const app = express();
+  { id: 1, title: 'ネーム', completed: false },
+  { id: 2, title: '下書き', completed: true }
+]
+const app = express()
 
-app.get('/api/todos', (req, res) => {
-    if(!req.query.completed){
-        return res.json(todos);
-    }
-
-    // GETのパラメータに'completed'がある場合
-    // 'completed=true'が来ていればtrue。
-    const completed =  req.query.completed === 'true'
-    res.json(todos.filter(todo => todo.completed === completed))
-});
-
-// SSE送信関数の保持配列
-let sseSenders = [];
-let sseId = 1;
+app.use(express.json())
 
 // TODO一覧の取得
-app.get( '/api/todos/events', (req, res) => {
-    // タイムアウトを抑止
-    req.socket.setTimeout(1000);
-    res.set({
-        // SSEのContent-Type
-        'Content-Type': 'text/event-stream'
-    });
+app.get('/api/todos', (req, res) => {
+  if (!req.query.completed) {
+    return res.json(todos)
+  }
 
-    // クライアントにSSEを送信する関数を登録。
-    const send = (id, data) => res.write(`id: ${id}\ndata: ${data}\n\n`);
-    sseSenders.push(send);
+  // GETのパラメータに'completed'がある場合
+  // 'completed=true'が来ていればtrue。
+  const completed = req.query.completed === 'true'
+  res.json(todos.filter(todo => todo.completed === completed))
+})
 
-    // リクエスト発生時点の状態を送信
-    send(sseId, JSON.stringify(todos));
+// SSE送信関数の保持配列
+let sseSenders = []
+let sseId = 1
 
-    // リクエストがクローズされたら、レスポンスをクローズして、SSE送信関数を配列から削除
-    req.on('close', ()=>{
-        res.end();
-        sseSenders = sseSenders.filter(_send => _send !== send);
-    });
-});
+// TODO一覧の取得（SSE）
+app.get('/api/todos/events', (req, res) => {
+  // タイムアウトを抑止
+  req.socket.setTimeout(0)
+  res.set({
+    // SSEのContent-Type
+    'Content-Type': 'text/event-stream'
+  })
 
-/** TODOの更新に伴い、全クライアントにSSEを送信する。 */
-function onUpdateTodos(){
-    sseId += 1;
-    const data = JSON.stringify(todos);
-    sseSenders.forEach(send => send(sseId, data));
+  // クライアントにSSEを送信する関数を登録。
+  const send = (id, data) => res.write(`id: ${id}\ndata: ${data}\n\n`)
+  sseSenders.push(send)
+
+  // リクエスト発生時点の状態を送信
+  send(sseId, JSON.stringify(todos))
+
+  // リクエストがクローズされたら、レスポンスをクローズして、SSE送信関数を配列から削除
+  req.on('close', () => {
+    res.end()
+    sseSenders = sseSenders.filter(_send => _send !== send)
+  })
+})
+
+/** ToDoの更新に伴い、全クライアントに対してSSEを送信する */
+function onUpdateTodos() {
+  sseId += 1
+  const data = JSON.stringify(todos)
+  sseSenders.forEach(send => send(sseId, data))
 }
 
-let id = todos.length;
+let id = todos.length
 
-// Todo登録
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.post('/api/todos', (req, res, next) =>{
-    res.setHeader('Content-Type', 'application/json');
-    console.log('POST');
-    console.log('req.body', req.body)
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.json())
 
-    const { title } = req.body;
-    if(typeof title !== 'string' || !title ){
-        // titleがリクエストに含まれていない場合はステータスコード400
-        const err = new Error('title is required.');
-        err.statusCode = 400;
-        return next(err);
-    }
+// Todo新規登録
+app.post('/api/todos', (req, res, next) => {
+  // res.setHeader('Content-Type', 'application/json')
+  // console.log('POST')
+  // console.log('req.body', req.body)
 
-    // Todoの作成
-    const todo = { id: id+=1 , title, completed:false };
-    todos.push(todo);
-    // 201(Created)
-    res.status(201).json(todo);
+  const { title } = req.body
+  if (typeof title !== 'string' || !title) {
+    // titleがリクエストに含まれていない場合はステータスコード400
+    const err = new Error('title is required')
+    err.statusCode = 400
+    return next(err)
+  }
 
-    // SSE送信
-    onUpdateTodos();
-});
+  // Todoの作成
+  const todo = { id: id += 1, title, completed: false }
+  todos.push(todo)
+  // 201(Created)
+  res.status(201).json(todo)
 
-// エラー制御
+  // SSE送信
+  onUpdateTodos()
+})
+
+// 指定されたIDのToDoを取得するためのミドルウェア
+app.use('/api/todos/:id(\\d+)', (req, res, next) => {
+  const targetId = Number(req.params.id)
+  const todo = todos.find(todo => todo.id === targetId)
+  if (!todo) {
+    const err = new Error('ToDo not found')
+    err.statusCode = 404
+    return next(err)
+  }
+  req.todo = todo
+  next()
+})
+
+// ToDoのCompletedの設定、解除
+app.route('/api/todos/:id(\\d+)/completed')
+  .put((req, res) => {
+    req.todo.completed = true
+    res.json(req.todo)
+    onUpdateTodos()
+  })
+  .delete((req, res) => {
+    req.todo.completed = false
+    res.json(req.todo)
+    onUpdateTodos()
+  })
+
+// ToDoの削除
+app.delete('/api/todos/:id(\\d+)', (req, res) => {
+  todos = todos.filter(todo => todo !== req.todo)
+  res.status(204).end()
+  onUpdateTodos()
+})
+
+// エラーハンドリングミドルウェア
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(err.statusCode || 500).json({error: err.message});
-});
+  console.error(err)
+  res.status(err.statusCode || 500).json({ error: err.message })
+})
 
-app.listen(3000);
+app.listen(3000)
 
-const next = require('next');
-const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({dev});
+const next = require('next')
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
 
 nextApp.prepare().then(
-    () => app.get('*', nextApp.getRequestHandler()),
-    err => {
-        console.err(err);
-        process.exit(1);
-    }
-);
+  // pagesディレクトリ内の各Reactコンポーネントに対するサーバサイドルーティング
+  () => app.get('*', nextApp.getRequestHandler()),
+  err => {
+    console.error(err)
+    process.exit(1)
+  }
+)
